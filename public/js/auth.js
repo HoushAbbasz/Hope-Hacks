ca206c4effa9d0a164df007179ffea52c4948346
mcastroal <marilyn.castroalonso@gmail.com>
2026-01-29 21:14:43 -0500

    deleted all code that had been commented out

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,25 +2,9 @@
 const API_URL = '/';
 
 const registerForm = document.getElementById('register-form');
-//const loginForm = document.getElementById('login-form');
-//const regUsername = document.getElementById("reg-username");
 const regEmail = document.getElementById("reg-email");
-//const regPassword = document.getElementById("reg-password");
-
-//const loginEmail = document.getElementById("login-email");
-//const loginPassword = document.getElementById("login-password");
-
-//const showLoginBtn = document.getElementById("show-login");
 const showRegisterBtn = document.getElementById("show-register");
 
-// if (sessionStorage.getItem("token")) {
-//   window.location.href = '/library';
-//}
-
-  // showLoginBtn.addEventListener("click", () => {
-  //   loginForm.classList.remove("hidden");
-  //   registerForm.classList.add("hidden");
-  // });
 
   showRegisterBtn.addEventListener("click", () => {
     registerForm.classList.remove("hidden");
@@ -45,28 +29,3 @@ registerForm.addEventListener("submit", async (e) => {
     alert("Registration failed");
   }
 });
-
-
-// Handle Login
-// loginForm.addEventListener("submit", async (e) => {
-//     e.preventDefault();
-
-//     const res = await fetch("/login", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//             email: loginEmail.value,
-//             password: loginPassword.value
-//         })
-//     });
-//     if (!res.ok) {
-//       alert("Login failed.");
-//       return;
-//     }
-//     const data = await res.json();
-//     // Store token
-//     sessionStorage.setItem("token", data.token);
-
-//     // Redirect to dashboard
-//     window.location.href = '/library';
-// });
